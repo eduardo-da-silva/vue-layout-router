2829da1b3d244a43cf249265aee9b0ccda1732a8
Eduardo da Silva <eduardo.silva@gmail.com>
2023-06-23 09:31:14 -0300

    Projeto inicial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <header>HEADER</header>
+  <aside>MENU</aside>
+  <main>MAIN</main>
+  <footer>FOOTER</footer>
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Sobre</h1>
+</template>
+
+<style></style>
